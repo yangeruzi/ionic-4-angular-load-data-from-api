861a12bcd8e92c28c4e3b9c7dca93e46521ac2f7
Nigel Peck <np.github@sharphosting.uk>
2019-06-17 23:10:45 -0500

    Prepare a container for the data and present it in the template

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  data: string;
 
-  constructor() {}
+  constructor() {
+    this.data = '';
+  }
 
 }
