bf8d825869af925c72d816b10aeccd8bcbbb7aa6
Nigel Peck <np.github@sharphosting.uk>
2019-06-20 11:20:19 -0500

    Prepare the API request and call it

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -11,6 +12,24 @@ export class HomePage {
 
   constructor(private http: HttpClient) {
     this.data = '';
+  }
+
+  ionViewWillEnter() {
+    // Load the data
+    this.prepareDataRequest()
+        .subscribe(
+            data => {
+              // Set the data to display in the template
+              this.data = JSON.stringify(data);
+            }
+        );
+  }
+
+  private prepareDataRequest(): Observable<object> {
+    // Define the data URL
+    const dataUrl = 'https://api.nigelbpeck.com/';
+    // Prepare the request
+    return this.http.get(dataUrl);
   }
 
 }
